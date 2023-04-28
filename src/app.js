69823aeafa13370099ee9635b37418fd61247431
GaboEh <105681841+GaboEh@users.noreply.github.com>
2023-04-28 17:40:10 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 //variables
-const PORT = process.env.PORT || 3050
+const PORT = process.env.PORT || 3050;
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_USER = process.env.DB_USER || 'root';
 const DB_PASSWORD = process.env.DB_PASSWORD || 'password';
