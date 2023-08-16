9ba42829c7ec02610801238c3c909a74bc98f763
GaboEh <105681841+GaboEh@users.noreply.github.com>
2023-08-16 09:53:36 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_USER = process.env.DB_USER || 'root';
 const DB_PASSWORD = process.env.DB_PASSWORD || 'password';
 const DB_NAME = process.env.DB_NAME || 'dblch';
-const DB_PORT = process.env.DB_PORT || 3306
+const DB_PORT = process.env.DB_PORT || 3306;
 
 const pool = mysql.createPool({
   host: DB_HOST,
